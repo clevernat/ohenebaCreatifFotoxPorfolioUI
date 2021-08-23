c10b5b68913f2debcad528fdb4d928c5dd2c2a81
clevernat <clevernat1@gmail.com>
2021-08-23 20:04:24 +0000

    Update: added typing effect and edit contact us button

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
+  
 
-  AOS.init();
+
+
+AOS.init();
 
 
 AOS.init({
@@ -27,6 +30,73 @@ AOS.init({
 
 
 
+
+
+
+  
+
+// // typing effect
+
+// var TxtRotate = function(el, toRotate, period) {
+//     this.toRotate = toRotate;
+//     this.el = el;
+//     this.loopNum = 0;
+//     this.period = parseInt(period, 10) || 2000;
+//     this.txt = '';
+//     this.tick();
+//     this.isDeleting = false;
+//   };
+  
+//   TxtRotate.prototype.tick = function() {
+//     var i = this.loopNum % this.toRotate.length;
+//     var fullTxt = this.toRotate[i];
+  
+//     if (this.isDeleting) {
+//       this.txt = fullTxt.substring(0, this.txt.length - 1);
+//     } else {
+//       this.txt = fullTxt.substring(0, this.txt.length + 1);
+//     }
+  
+//     this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  
+//     var that = this;
+//     var delta = 300 - Math.random() * 100;
+  
+//     if (this.isDeleting) { delta /= 2; }
+  
+//     if (!this.isDeleting && this.txt === fullTxt) {
+//       delta = this.period;
+//       this.isDeleting = true;
+//     } else if (this.isDeleting && this.txt === '') {
+//       this.isDeleting = false;
+//       this.loopNum++;
+//       delta = 500;
+//     }
+  
+//     setTimeout(function() {
+//       that.tick();
+//     }, delta);
+//   };
+  
+//   window.onload = function() {
+//     var elements = document.getElementsByClassName('txt-rotate');
+//     for (var i=0; i<elements.length; i++) {
+//       var toRotate = elements[i].getAttribute('data-rotate');
+//       var period = elements[i].getAttribute('data-period');
+//       if (toRotate) {
+//         new TxtRotate(elements[i], JSON.parse(toRotate), period);
+//       }
+//     }
+//     // INJECT CSS
+//     var css = document.createElement("style");
+//     css.type = "text/css";
+//     css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+//     document.body.appendChild(css);
+//   };
+
+
+ 
+  
 
 
   
@@ -70,3 +140,57 @@ function dates() {
       document.querySelector('.insert-date').innerHTML = `Dec ${year}`;
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// typing effect
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["A Professional Graphic Designer.", "The UI/UX Designer."];
+const typingDelay = 150;
+const erasingDelay = 100;
+const newTextDelay = 1500; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+  	setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+	if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
